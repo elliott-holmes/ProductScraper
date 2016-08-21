ce1484194ebb992315b91c6a83e57fecf7be67ca
holmese <holmese@EH-CSI-W520.uk.csi>
2016-08-21 17:39:55 +0100

    Commit with initial stages of URL reader stages

---
+++ ProductReader/src/holmes/elliott/productreader/ProductReader.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package holmes.elliott.productreader;
+
+/**
+ * @author holmese
+ *
+ */
+public class ProductReader {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Running Code");
+
+	}
+
+}
